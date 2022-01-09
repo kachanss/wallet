3942f9d18cd8f8cf2b5da709b981dd39a61cd49e
Kachan Sergey <s.kachan@silencatech.com>
2022-01-09 17:21:34 +0200

    Update Settings screen

--- app/components/Settings/Settings.tsx
+++ app/components/Settings/Settings.tsx
@@ -1,91 +1,152 @@
-import CommonLayout from '../Layout/CommonLayout';
 import {SectionList, TouchableOpacity, View} from 'react-native';
-import {Text, Header} from 'react-native-elements';
+import {Text} from 'react-native-elements';
 import React from 'react';
+import CommonLayout from '../Layout/CommonLayout';
 
-type MenuItem = {
-  key: (index: number) => string;
-  render: () => React.ReactElement;
+type InfoItemProps = {
+  info: string;
 };
 
-class InfoItem implements MenuItem {
-  info: string;
-
-  constructor(info: string) {
-    this.info = info;
-  }
-
-  key(index: number): string {
-    return 'simple_' + index;
-  }
-
-  render(): React.ReactElement {
-    return (
-      <View
-        style={{
-          padding: 25,
-          paddingLeft: 8,
-          borderBottomWidth: 1,
-          borderBottomColor: '#eeeeee',
-          backgroundColor: 'white',
-        }}>
-        <Text>{this.info}</Text>
-      </View>
-    );
-  }
-}
-
-class TouchableInfoItem extends InfoItem {
+type PressableInfoItemProps = InfoItemProps & {
   onPress: () => void;
+};
 
-  constructor(info: string, onPress: () => void) {
-    super(info);
-    this.onPress = onPress.bind(this);
-  }
+type CopiableInfoItemProps = Omit<PressableInfoItemProps, 'onPress'> & {
+  copyText: string | null;
+};
 
-  render(): React.ReactElement {
-    return (
-      <TouchableOpacity onPress={this.onPress}>
-        {super.render()}
-      </TouchableOpacity>
-    );
-  }
+const InfoItem = ({info}: InfoItemProps) => {
+  return (
+    <View
+      style={{
+        padding: 25,
+        paddingLeft: 8,
+        borderBottomWidth: 1,
+        borderBottomColor: '#eeeeee',
+        backgroundColor: 'white',
+      }}>
+      <Text>{info}</Text>
+    </View>
+  );
+};
+
+const PressableInfoItem = ({info, onPress}: PressableInfoItemProps) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <InfoItem info={info} />
+    </TouchableOpacity>
+  );
+};
+
+const CopiableInfoItem = ({info, copyText = null}: CopiableInfoItemProps) => {
+  const handleCopy = () => {
+    const text = copyText === null ? info : copyText;
+    console.log('Copied: ', text);
+  };
+
+  return <PressableInfoItem info={info} onPress={handleCopy} />;
+};
+
+enum ItemType {
+  Info = 'info',
+  PressableInfo = 'info_press',
+  CopiableInfo = 'info_copy',
 }
 
-class CopiableInfoItem extends TouchableInfoItem {
-  constructor(info: string, copyText: string | null = null) {
-    if (copyText === null) {
-      copyText = info;
-    }
-    super(info, () => console.log('Copied: ', copyText));
-  }
-}
+type SectionHeaderProps = {
+  title: string;
+};
+
+const SectionHeader = ({title}: SectionHeaderProps) => {
+  return (
+    <View
+      style={{
+        padding: 10,
+        paddingLeft: 8,
+        backgroundColor: 'white',
+      }}>
+      <Text style={{fontSize: 20}}>{title}</Text>
+    </View>
+  );
+};
 
 const DATA = [
-  {title: 'Wallets', data: [new InfoItem('Choose account'), new InfoItem('Backup account')]},
-  {title: 'Security', data: [new InfoItem('Set PIN'), new InfoItem('Enable biometrics')]},
-  {title: 'Common', data: [new InfoItem('Language'), new InfoItem('Local currency'), new CopiableInfoItem('Version: 1.2.3', '1.2.3'), new TouchableInfoItem('Support', () => console.log('Support pressed'))]},
+  {
+    title: 'Wallets',
+    data: [
+      {type: ItemType.Info, info: 'Choose account', key: 'wallets_choose'},
+      {type: ItemType.Info, info: 'Backup account', key: 'wallets_backup'},
+    ],
+  },
+  {
+    title: 'Security',
+    data: [
+      {type: ItemType.Info, info: 'Set PIN', key: 'security_set_pin'},
+      {
+        type: ItemType.Info,
+        info: 'Enable biometrics',
+        key: 'security_biometrics',
+      },
+    ],
+  },
+  {
+    title: 'Common',
+    data: [
+      {type: ItemType.Info, info: 'Language', key: 'common_lang'},
+      {type: ItemType.Info, info: 'Local currency', key: 'common_currency'},
+      {
+        type: ItemType.CopiableInfo,
+        info: 'Version: 1.2.3',
+        copyText: '1.2.3',
+        key: 'common_version',
+      },
+      {
+        type: ItemType.PressableInfo,
+        info: 'Support',
+        onPress: () => console.log('Support pressed'),
+        key: 'common_support',
+      },
+    ],
+  },
 ];
 
-const Item = ({title}: any) => <Text>{title}</Text>;
+type ItemTypeAware = {
+  type: ItemType;
+};
+
+type KeyAware = {
+  key: string;
+};
 
 const Settings = () => {
+  const renderItem = ({item}: {item: ItemTypeAware & KeyAware}) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {type, key, ...rest} = item;
+
+    switch (item.type) {
+      case ItemType.Info:
+        // @ts-ignore
+        return <InfoItem {...rest} />;
+      case ItemType.PressableInfo:
+        // @ts-ignore
+        return <PressableInfoItem {...rest} />;
+      case ItemType.CopiableInfo:
+        // @ts-ignore
+        return <CopiableInfoItem {...rest} />;
+      default:
+        console.log(`Unsupported ItemType (${item.type})`);
+        return null;
+    }
+  };
+
   return (
     <CommonLayout showFooter={true}>
       <SectionList
         style={{backgroundColor: 'white'}}
         sections={DATA}
-        keyExtractor={(item: MenuItem, index) => item.key(index)}
-        renderItem={({item}: {item: MenuItem}) => item.render()}
+        renderItem={renderItem}
         renderSectionHeader={({section: {title}}) => (
-          <View
-            style={{
-              padding: 10,
-              paddingLeft: 8,
-              backgroundColor: 'white',
-            }}>
-            <Text style={{fontSize: 20}}>{title}</Text>
-          </View>
+          <SectionHeader title={title} />
         )}
       />
     </CommonLayout>
